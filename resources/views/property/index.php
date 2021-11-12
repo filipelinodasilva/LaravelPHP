0e1fb6554eec4ba96027b552aeac5e67b2779253
filipelinodasilva <filipelinodasilva@hotmail.com>
2021-11-12 12:08:29 -0300

    CRUD - Formatando Tabelas

--- resources/views/property/index.php
+++ resources/views/property/index.php
@@ -8,18 +8,18 @@ if(!empty($properties)){
 
     echo "<table>";
 
-    echo "<tr>
-            <td>Título</td>
-            <td>Valor de Locação</td>
-            <td>Valor de Compra</td>
-        </tr>";
+        echo "<tr>
+                 <td>Título</td>
+                 <td>Valor de Locação</td>
+                 <td>Valor de Compra</td>
+              </tr>";
 
     foreach($properties as $property){
         echo "<tr>
-            <td>{$property->title}</td>
-            <td>{$property->rental_price}</td>
-            <td>{$property->sale_price}</td>
-             </tr>";
+                 <td>{$property->title}</td>
+                 <td>{$property->rental_price}</td>
+                 <td>{$property->sale_price}</td>
+              </tr>";
     }
         echo "</table>";
 
